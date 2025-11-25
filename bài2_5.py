c318ca82ab8f9017feb0402c84a770976badc5ef
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 10:56:18 +0700

    Add files via upload

---
+++ bài2_5.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+n = int(input("Enter a number (n > 0): "))
+
+while n >= 0:
+    print(n)
+    n = n - 1
