43aa0219948ef4ab5b15941d6e16d00d74836de7
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 11:03:37 +0700

    bai2_7

---
+++ bài2_7.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+n = int(input("Nhập vào một số: "))
+d = dict()
+
+for i in range(1, n + 1):
+    d[i] = i * i
