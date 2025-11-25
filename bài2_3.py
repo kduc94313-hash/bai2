fac0d8f9184ca81f9eccd4e6c73fec3ef71529e3
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 10:50:56 +0700

    Add files via upload

---
+++ bài2_3.py
@@ -0,0 +1,10 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+n = int(input("Enter a number ---> "))
+
+if n % 2 == 0:
+    print("EVEN Number")
+else:
+    print("ODD Number")
