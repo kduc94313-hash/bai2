6cb3842db6f704a3a1b87bb6cfd035dc0bf22a04
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 11:07:16 +0700

    bai2_9

---
+++ bài2_9.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+s = input("Enter a string: ")
+d = {}
+
+for ch in s:
+    d[ch] = s.count(ch)
