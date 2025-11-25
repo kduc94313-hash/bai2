58eb2fef75e8ceb49553d5874f9ccb44618d55fa
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 11:09:47 +0700

    bai2_10

---
+++ bài2_10.py
@@ -0,0 +1,11 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+a = "Hi I am python programmer"
+
+b = a.split()     
+print("After split:", b)
+
+c = "-".join(b)    
+print("After join:", c)
