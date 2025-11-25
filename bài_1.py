779e77e221ceab9b7d9ae71fddfa21af81fa13e8
kduc94313-hash <kduc94313@gmail.com>
2025-11-25 10:27:54 +0700

    Add files via upload

---
+++ bài_1.py
@@ -0,0 +1,12 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+n1 = int(input("Enter n1 value: "))
+n2 = int(input("Enter n2 value: "))
+
+if n1 > n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
+
